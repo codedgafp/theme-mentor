5ea5138759d9c6b53180eb51691f9486c60cff33
aplo <alban.ploquin@cgi.com>
2025-01-29 15:58:56 +0100

    fix(usertour): #MEN-426: fix modal positionning

--- javascript/usertours.js
+++ javascript/usertours.js
@@ -1,7 +1,29 @@
 $(document).ready(function () {
     // Close usertours pop-up when user click to the "X".
-    $(document)
-        .on('click', 'span[data-flexitour] button.close', function () {
-            $('span[data-flexitour] .modal-footer button[data-role="end"]').click();
+    $(document).on('click', 'span[data-flexitour] button.close', function () {
+        $('span[data-flexitour] .modal-footer button[data-role="end"]').click();
+    });
+});
+
+// Create an observer to monitor changes in the DOM
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(function(node) {
+            if (node.matches && node.matches('span[data-flexitour="container"].orphan')) {
+                let offset = $(document).scrollTop(),
+
+                viewportHeight = $(window).height();
+
+                node.css('top',  (offset  + (viewportHeight/2)) - (node.outerHeight()/2));
+
+                observer.disconnect();
+            }
         });
+    });
+});
+
+// Observer configuration
+observer.observe(document.body, {
+    childList: true,    // Observe all added/deleted children
+    subtree: true       // Observe the entire DOM
 });
