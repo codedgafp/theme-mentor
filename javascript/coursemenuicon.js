ee002b2f8855d9d1dc7d15595583e7927525b9b7
sben <sana.benhammamia@cgi.com>
2025-08-13 14:50:24 +0200

    feat(action-menu-icon):#MEN-779 change menu action icon

--- javascript/coursemenuicon.js
+++ javascript/coursemenuicon.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('div.context-header-settings-menu i.icon.fa-ellipsis-vertical').forEach(function (icon) {
+    document.querySelectorAll('a[id^="action-menu-toggle-"] i.icon.fa-ellipsis-vertical').forEach(function (icon) {
         icon.classList.remove('fa-ellipsis-vertical');
         icon.classList.add('fa-cog');
     });
