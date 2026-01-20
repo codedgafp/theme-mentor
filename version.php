13a47b4cc3036cdbaa0a4fe2e4c14b0e38c7b434
alap <agnes.lapeyronnie@cgi.com>
2026-01-20 14:26:28 +0100

    feat(static page): #MEN-1030 replace "FAQ" with "Aide" on footer

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2026010500; 
+$plugin->version = 2026012000; 
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2016112900.00;
