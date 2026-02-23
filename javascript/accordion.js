5135c6c63c37c52f5fc5b9489b2dd0548bccd352
Alban PLOQUIN <alban.ploquin@cgi.com>
2026-02-23 15:50:20 +0100

    fix(snippet accordion): #MEN-1155: detects when a new accordion is dynamically added

--- javascript/accordion.js
+++ javascript/accordion.js
@@ -13,131 +13,55 @@ $(document).ready(function () {
         }
     });
 
-    
-
     // Set collapse position with set browser mobile size.
-    $('body .mentor-accordion .card-header').each(function (index, element) {
-        // When Collapse isn't in the atto snippet editor
-        if (!$(element).parents('.editor_atto_content').length) {
+    $("body .mentor-accordion .card-header").each(set_collapse_mobile_size);
 
-            // Get target collapse.
-            var collapseTarget = element.parentElement.parentElement;
-
-            // Get target collapse element.
-            var contentTarget = $(element.nextElementSibling, collapseTarget);
-
-            // Get collapse mobile size data when page is loaded.
-            var mobileSize = contentTarget.data('collapseMobileSize');
-
-            // Is not mobile size.
-            if (window.innerWidth > mobileSize) {
-                return null;
-            }
-
-            // Get collapse mobile action data when page is loaded.
-            var mobileAction = contentTarget.data('collapseMobileAction');
-
-            // Collapse is close when browser has mobile size.
-            if (mobileAction === 'close') {
-
-                // Hidden target element.
-                contentTarget.removeClass('show');
-
-                // Remove 'opened' class to target collapse element header.
-                $(element).removeClass('opened');
-
-                // Set aria expanded to false.
-                $(element).attr('aria-expanded', "false");
-                $(element).find('button').attr('aria-expanded', "false");
-
-                // Change header right target collapse element to close.
-                $('.header-right', element).html(chevronDown);
-            }
-
-            // Collapse is open when browser has mobile size.
-            if (mobileAction === 'open') {
-                // Show target element.
-                contentTarget.addClass('show');
-
-                // Add 'opened' class to target collapse element header.
-                contentTarget.addClass('opened');
-
-                // Set aria expanded to true.
-                $(element).attr('aria-expanded', "true");
-                $(element).find('button').attr('aria-expanded', "true");
-
-                // Change header right target collapse element indicator to open.
-                $('.header-right', element).html(chevronUp);
-            }
-        }
-    });
-
-    $('body .mentor-accordion').on('click', '.card-header', function (event) {
+    $("body").on("click", ".mentor-accordion .card-header", function (event) {
         event.preventDefault();
 
-        // When Collapse isn't in the atto snippet editor
-        if (!$(event.currentTarget).parents('.editor_atto_content').length) {
-
-            // Get target collapse
+        if ($(event.currentTarget).parents('.editor_atto_content').length == false) {
             var collapseTarget = event.currentTarget.parentElement.parentElement;
-
-            // Get target collapse element
             var contentTarget = $(event.currentTarget.nextElementSibling, collapseTarget);
 
-            // When target element is opened
             if (contentTarget.hasClass('show')) {
-
-                // Hidden target element
                 contentTarget.removeClass('show');
-
-                // Remove 'opened' class to target collapse element header
                 $(event.currentTarget).removeClass('opened');
 
-                // Set aria expanded to false.
                 $(event.currentTarget).attr('aria-expanded', "false");
                 $(event.currentTarget).find('button').attr('aria-expanded', "false");
 
-                // Change header right target collapse element to close
                 $('.header-right', event.currentTarget).html(chevronDown);
-            } else {// When target element is closed
+            } else {
                 contentTarget.addClass('show');
-
-                // Change header right target collapse element indicator to open
                 $('.header-right', event.currentTarget).html(chevronUp);
 
-                // Add 'opened' class to target collapse element header
                 if (!$(event.currentTarget).hasClass('opened')) {
                     $(event.currentTarget).addClass('opened');
 
-                    // Set aria expanded to true.
                     $(event.currentTarget).attr('aria-expanded', "true");
                     $(event.currentTarget).find('button').attr('aria-expanded', "true");
                 }
 
-                // Check all element in target collapse
                 $('.collapse', collapseTarget).each(function (index, content) {
-
-                    // Check if is not target collapse element
                     if (!$(content).is(contentTarget)) {
-
-                        // Hidden other collapse element
                         $(content).removeClass('show');
-
-                        // Change header right other collapse element indicator to open
                         $('.header-right', $(content).prev()).html(chevronDown);
 
-                        // Remove 'opened' to class other collapse element header
                         if ($(content).prev().hasClass('opened')) {
                             $(content).prev().removeClass('opened').attr('aria-expanded', "false");
                         }
                     }
                 });
-
             }
         }
     });
 
     $(window).bind('load', function () {
+        attoEditorObserver();
+        newAccordionObserver();
+    });
+
+    function attoEditorObserver() {
         let attoEditors = $('.editor_atto_content');
         if (attoEditors.length === 0) return;
 
@@ -161,7 +85,21 @@ $(document).ready(function () {
         attoEditors.find(".mentor-accordion").each(function () {
             initAccordion($(this));
         });
-    });
+    }
+
+    function newAccordionObserver() {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(function (mutation) {
+                mutation.addedNodes.forEach(function (node) {
+                    $(node).find('.mentor-accordion .card-header').each(set_collapse_mobile_size);
+                });
+            });
+        });
+
+        const config = { childList: true, subtree: true };
+
+        observer.observe($("body")[0], config);
+    }
 
     function initAccordion(accordion) {
         accordion.find("div.card").each(function () {
@@ -186,11 +124,11 @@ $(document).ready(function () {
         let collapseUid = generateUID();
         let buttonUid = generateUID();
         card.find("div.collapse-content")
-        .attr('id', collapseUid)
-        .attr('aria-labelledby', buttonUid);
+            .attr('id', collapseUid)
+            .attr('aria-labelledby', buttonUid);
         card.find("a.card-header")
-        .attr('id', buttonUid)
-        .attr('aria-controls', collapseUid);
+            .attr('id', buttonUid)
+            .attr('aria-controls', collapseUid);
     }
 
 
@@ -198,4 +136,37 @@ $(document).ready(function () {
         return Date.now().toString(36) + Math.random().toString(36).substring(2, 7);
     }
 
+    function set_collapse_mobile_size(index, element) {
+        if ($(element).parents('.editor_atto_content').length == false) {
+            var collapseTarget = element.parentElement.parentElement;
+            var contentTarget = $(element.nextElementSibling, collapseTarget);
+            var mobileSize = contentTarget.data('collapseMobileSize');
+
+            if (window.innerWidth > mobileSize) {
+                return null;
+            }
+
+            var mobileAction = contentTarget.data('collapseMobileAction');
+
+            if (mobileAction === 'close') {
+                contentTarget.removeClass('show');
+                $(element).removeClass('opened');
+
+                $(element).attr('aria-expanded', "false");
+                $(element).find('button').attr('aria-expanded', "false");
+
+                $('.header-right', element).html(chevronDown);
+            }
+
+            if (mobileAction === 'open') {
+                contentTarget.addClass('show');
+                contentTarget.addClass('opened');
+
+                $(element).attr('aria-expanded', "true");
+                $(element).find('button').attr('aria-expanded', "true");
+
+                $('.header-right', element).html(chevronUp);
+            }
+        }
+    }
 });
--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2026012000; 
+$plugin->version = 2026022300; 
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2016112900.00;
